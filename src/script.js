4a6c1b8b977130bdbb843b09753d0b85086f9a93
Elizabeth-Susan <74855891+Elizabeth-Susan@users.noreply.github.com>
2021-05-21 22:33:14 +0300

    Changed no. of items as per resolution

--- src/script.js
+++ src/script.js
@@ -8,11 +8,11 @@ $(".owl-carousel").owlCarousel({
     0: {
       items: 1,
     },
-    400: {
+    500: {
       items: 2,
     },
-    1000: {
-      items: 2,
+    1200: {
+      items: 3,
     },
   },
 });
